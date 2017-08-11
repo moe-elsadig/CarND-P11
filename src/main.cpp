3b0d44a6f9f53f399dd6f445515f8ead49342fb0
moe-elsadig <m.da7th@gmail.com>
2017-08-11 17:12:46 +0300

    update: added a constant speed to the vehicle to move straight in the simulator

--- src/main.cpp
+++ src/main.cpp
@@ -209,12 +209,12 @@ int main() {
 
       if (s != "") {
         auto j = json::parse(s);
-        
+
         string event = j[0].get<string>();
-        
+
         if (event == "telemetry") {
           // j[1] is the data JSON object
-          
+
         	// Main car's localization Data
           	double car_x = j[1]["x"];
           	double car_y = j[1]["y"];
@@ -226,7 +226,7 @@ int main() {
           	// Previous path data given to the Planner
           	auto previous_path_x = j[1]["previous_path_x"];
           	auto previous_path_y = j[1]["previous_path_y"];
-          	// Previous path's end s and d values 
+          	// Previous path's end s and d values
           	double end_path_s = j[1]["end_path_s"];
           	double end_path_d = j[1]["end_path_d"];
 
@@ -238,6 +238,16 @@ int main() {
           	vector<double> next_x_vals;
           	vector<double> next_y_vals;
 
+            double dist_inc = 0.5;
+            for(int i=0; i<50; i++){
+
+              next_x_vals.push_back(car_x + (dist_inc*i) * cos(deg2rad(car_yaw)));
+              next_y_vals.push_back(car_y + (dist_inc*i) * sin(deg2rad(car_yaw)));
+            }
+
+
+
+
 
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
           	msgJson["next_x"] = next_x_vals;
@@ -247,7 +257,7 @@ int main() {
 
           	//this_thread::sleep_for(chrono::milliseconds(1000));
           	ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-          
+
         }
       } else {
         // Manual driving
@@ -290,83 +300,3 @@ int main() {
   }
   h.run();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
